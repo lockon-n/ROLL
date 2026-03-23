bc99da2ed735093197535244a8f980d2543bb2b3
junlong.li <junlong.li@bytedance.com>
2026-03-23 15:46:42 +0000

    add setup

--- jl_patch_tt/run_agentic_sokoban.sh
+++ jl_patch_tt/run_agentic_sokoban.sh
@@ -5,6 +5,14 @@ unset VIRTUAL_ENV
 
 # pip install dacite
 # pip install imageio
+# pip install gem-llm==0.0.4
+# pip install transformers==5.2.0
+# pip install flash-linear-attention==0.4.2
+# pip uninstall byted-wandb
+# pip uninstall bytedray
+# pip install ray
+# pip install "antlr4-python3-runtime==4.9.3" "latex2sympy2_extended==1.10.1"
+# pip install -e mcore_adapter
 
 # Stop any residual Ray cluster to avoid stale GPU mappings
 ray stop 2>/dev/null
