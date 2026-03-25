ff7ad98f2731c86a8a29f54976ba6dd3b499a01e
junlong.li <junlong.li@bytedance.com>
2026-03-25 12:17:55 +0000

    update

---
+++ jl_patch_tt/setup.sh
@@ -0,0 +1,10 @@
+sudo pip install modelscope==1.35.1
+sudo pip install dacite==1.9.2
+sudo pip install imageio==2.37.3
+sudo pip install gem-llm==0.0.4 --ignore-requires-python
+sudo pip install transformers==5.2.0
+sudo pip install flash-linear-attention==0.4.2
+sudo pip uninstall byted-wandb -y && sudo pip install wandb
+sudo pip uninstall bytedray -y && sudo pip install ray==2.54.0
+sudo pip install "antlr4-python3-runtime==4.9.3" "latex2sympy2_extended==1.10.1"
+pip install -e ./mcore_adapter
