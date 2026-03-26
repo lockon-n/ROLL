f1fbe354f4007246bdbdf7e85156712fc2099855
lockon-n <dd098309@126.com>
2026-03-26 21:03:49 +0800

    update

--- jl_patch_tt/run_agentic_sokoban_multinode.sh
+++ jl_patch_tt/run_agentic_sokoban_multinode.sh
@@ -11,4 +11,10 @@ export WORKER_NUM=$ARNOLD_WORKER_NUM
 export MASTER_ADDR=$ARNOLD_WORKER_0_HOST
 export MASTER_PORT=$(echo $ARNOLD_WORKER_0_PORT | cut -d',' -f1)
 
+echo "RANK: $RANK"
+echo "WORLD_SIZE: $WORLD_SIZE"
+echo "WORKER_NUM: $WORKER_NUM"
+echo "MASTER_ADDR: $MASTER_ADDR"
+echo "MASTER_PORT: $MASTER_PORT"
+
 python examples/start_agentic_pipeline.py --config_path ../jl_patch_tt --config_name qwen3_5_0_8B_agentic_sokoban_multinode
