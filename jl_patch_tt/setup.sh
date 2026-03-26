d14c0440968ef2e9839b137c492d733f16aecf94
lockon-n <dd098309@126.com>
2026-03-26 17:14:52 +0800

    fix dependency

--- jl_patch_tt/setup.sh
+++ jl_patch_tt/setup.sh
@@ -8,5 +8,5 @@ sudo pip install flash-linear-attention==0.4.2
 sudo pip uninstall bytedray -y && sudo pip install ray==2.54.0
 sudo pip install "antlr4-python3-runtime==4.9.3" "latex2sympy2_extended==1.10.1"
 sudo pip install gym-sokoban==0.0.6
-sudo pip install gymnasium==1.2.3
+sudo pip install gymnasium[toy-text]==1.2.3
 pip install -e ./mcore_adapter
