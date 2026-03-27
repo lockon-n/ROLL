2ec4bd7ce104857668b3b79c53c0fbb66bdf1cfd
lockon-n <dd098309@126.com>
2026-03-27 15:20:42 +0800

    update

--- playground/fastrun.sh
+++ playground/fastrun.sh
@@ -1,1 +1,1 @@
-curl http://localhost:8000/v1/chat/completions -H "Content-Type: application/json" -d '{"model":"Qwen3-VL-2B-Instruct","messages":[{"role":"user","content":"Hello, who are you?"}],"max_tokens":128,"stream":true}'
+curl http://localhost:8000/v1/chat/completions -H "Content-Type: application/json" -d '{"model":"Qwen3-VL-2B-Instruct","messages":[{"role":"user","content":"Hello, who are you?"}],"max_tokens":128}'
