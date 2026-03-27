fb51444cb626b40c459c1f5c9e5a8fbd9bd79e74
lockon-n <dd098309@126.com>
2026-03-27 17:03:31 +0800

    update

--- jl_patch_tt/fix_vllm_qwen3_5.sh
+++ jl_patch_tt/fix_vllm_qwen3_5.sh
@@ -12,7 +12,7 @@ fi
 
 if grep -q 'kwargs\["ignore_keys_at_rope_validation"\] = \[' "$TARGET"; then
     sed -i 's/kwargs\["ignore_keys_at_rope_validation"\] = \[/kwargs["ignore_keys_at_rope_validation"] = {/' "$TARGET"
-    sed -i '/kwargs\["ignore_keys_at_rope_validation"\] = {/{n;n;s/\]/}/}' "$TARGET"
+    sed -i 's/^\(\s*\)\]$/\1}/' "$TARGET"
     echo "Fixed: $TARGET"
     sed -n '/ignore_keys_at_rope_validation/,+3p' "$TARGET"
 else
