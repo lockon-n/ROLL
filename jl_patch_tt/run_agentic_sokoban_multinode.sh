2e86d4785b465772142a04a597a01d733022811f
lockon-n <dd098309@126.com>
2026-03-30 20:21:37 +0800

    update

--- jl_patch_tt/run_agentic_sokoban_multinode.sh
+++ jl_patch_tt/run_agentic_sokoban_multinode.sh
@@ -4,5 +4,6 @@ set +x
 unset VIRTUAL_ENV
 
 export ROLL_RAY_RUNTIME_ENV=1
+export ROLL_PLATFORM=cuda
 
 python examples/start_agentic_pipeline.py --config_path ../jl_patch_tt --config_name qwen3_5_0_8B_agentic_sokoban_multinode
