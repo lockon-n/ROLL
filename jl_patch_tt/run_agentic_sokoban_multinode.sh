e348f0fb5f240386e68ec18bb53b9a8989ed73a1
lockon-n <dd098309@126.com>
2026-03-30 19:40:11 +0800

    update

--- jl_patch_tt/run_agentic_sokoban_multinode.sh
+++ jl_patch_tt/run_agentic_sokoban_multinode.sh
@@ -2,19 +2,7 @@
 set +x
 
 unset VIRTUAL_ENV
-export ROLL_PLATFORM=cuda
-# export NNODES=$ARNOLD_WORKER_NUM
-# export NODE_RANK=$ARNOLD_ID
-export RANK=$ARNOLD_ID
-export WORLD_SIZE=$ARNOLD_WORKER_NUM
-export WORKER_NUM=$ARNOLD_WORKER_NUM
-export MASTER_ADDR=[${ARNOLD_WORKER_0_HOST}]
-# export MASTER_PORT=$(echo $ARNOLD_WORKER_0_PORT | cut -d',' -f1)
 
-echo "RANK: $RANK"
-echo "WORLD_SIZE: $WORLD_SIZE"
-echo "WORKER_NUM: $WORKER_NUM"
-echo "MASTER_ADDR: $MASTER_ADDR"
-echo "MASTER_PORT: $MASTER_PORT"
+export ROLL_RAY_RUNTIME_ENV=1
 
 python examples/start_agentic_pipeline.py --config_path ../jl_patch_tt --config_name qwen3_5_0_8B_agentic_sokoban_multinode
