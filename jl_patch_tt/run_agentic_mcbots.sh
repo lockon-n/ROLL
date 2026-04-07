060d3345ffa10a2216f89ede99daa9d279dafc1c
junlong.li <junlong.li@bytedance.com>
2026-04-07 13:25:17 +0000

    update

--- jl_patch_tt/run_agentic_mcbots.sh
+++ jl_patch_tt/run_agentic_mcbots.sh
@@ -7,5 +7,6 @@ unset VIRTUAL_ENV
 ray stop 2>/dev/null
 
 export ROLL_RAY_RUNTIME_ENV=0
+export MCBOTS_INITIAL_USER_INPUT="explore freely!"
 
-python examples/start_agentic_pipeline.py --config_path ../jl_patch_tt --config_name qwen3_5_0_8B_agentic_mcbots
+python examples/start_agentic_pipeline.py --config_path ../jl_patch_tt --config_name qwen3_5_4B_agentic_mcbots
