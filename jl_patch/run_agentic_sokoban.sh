f1251851f4732ac456a06071062146c1b861f681
lockon-n <dd098309@126.com>
2026-04-08 14:46:50 +0800

    update some local scripts

--- jl_patch/run_agentic_sokoban.sh
+++ jl_patch/run_agentic_sokoban.sh
@@ -19,7 +19,7 @@ export TMPDIR=/ssddata/junlong/tmp
 export CUDA_HOME=~/cuda-12.9
 export LD_LIBRARY_PATH=~/cuda-12.9/lib64:/export/ssddata/junlong/projects/ROLL/.venv/lib/python3.12/site-packages/nvidia/cudnn/lib:/export/ssddata/junlong/projects/ROLL/.venv/lib/python3.12/site-packages/nvidia/nccl/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 export WANDB_API_KEY=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
-export ROLL_RAY_RUNTIME_ENV=1
+export ROLL_RAY_RUNTIME_ENV=0
 export MASTER_PORT=${MASTER_PORT}
 export DASHBOARD_PORT=${DASHBOARD_PORT}
 
@@ -28,4 +28,6 @@ echo "DASHBOARD_PORT: ${DASHBOARD_PORT}"
 
 ray stop 2>/dev/null
 
-uv run examples/start_agentic_pipeline.py --config_path ../jl_patch --config_name qwen3_5_0_8B_agentic_sokoban
+echo "ROLL_RAY_RUNTIME_ENV: ${ROLL_RAY_RUNTIME_ENV}"  
+
+uv run examples/start_agentic_pipeline.py --config_path ../jl_patch --config_name qwen3_5_4B_agentic_sokoban
