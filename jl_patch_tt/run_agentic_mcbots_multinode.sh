648a11c8480399e2b35ce2c431557b2f06aab2a1
lockon-n <dd098309@126.com>
2026-04-15 17:05:40 +0800

    update

--- jl_patch_tt/run_agentic_mcbots_multinode.sh
+++ jl_patch_tt/run_agentic_mcbots_multinode.sh
@@ -33,4 +33,4 @@ export MC_SERVER_PORT=$(python3 -c "import json; print(json.load(open('${mc_serv
 echo "MC_SERVER_HOST: ${MC_SERVER_HOST}"
 echo "MC_SERVER_PORT: ${MC_SERVER_PORT}"
 
-python examples/start_agentic_pipeline.py --config_path ../jl_patch_tt --config_name qwen3_5_9B_agentic_mcbots_multinode
+python examples/start_agentic_pipeline.py --config_path ../jl_patch_tt --config_name qwen3_5_4B_agentic_mcbots_multinode
